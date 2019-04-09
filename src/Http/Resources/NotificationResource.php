453abddb9962758cf628cbfeabd248643e8df227
Victoria <dev@coders.studio>
2019-04-09 13:54:18 +0300

    check props

--- src/Http/Resources/NotificationResource.php
+++ src/Http/Resources/NotificationResource.php
@@ -2,6 +2,7 @@
 namespace CodersStudio\Notifications\Http\Resources;
 use Illuminate\Http\Resources\Json\Resource;
 use function GuzzleHttp\json_decode;
+use Carbon\Carbon;
 class NotificationResource extends Resource
 {
 
@@ -18,11 +19,12 @@ class NotificationResource extends Resource
             'type'          => 'notifications',
             'id'            => (string)$this->id,
             'attributes'    => [
-                'text' =>  $this->data['text'],
-                'params' =>  $this->data['params'],
+                'text' =>  $this->data['text'] ?? null,
+                'date' =>  Carbon::parse($this->created_at)->format('d.m.Y h:i'),
+                'params' =>  $this->data['params'] ?? null,
             ],
             'links'         => [
-                'self' => $this->data['link'],
+                'self' => $this->data['link'] ?? null,
             ],
         ];
     }
