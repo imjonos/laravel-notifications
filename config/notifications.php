cfb9ffa44deaa50110dda84347cf4b7ac4b83a50
Eugeny Nosenko <info@coders.studio>
2020-03-25 11:53:18 +0300

    О да

--- config/notifications.php
+++ config/notifications.php
@@ -2,6 +2,5 @@
 
 return [
     'model' => \App\User::class,
-    'system_notification' => 'CodersStudio\Notifications\Notifications\System',
-    'telegram_chat_id' => env('TELEGRAM_CHAT_ID',null)
+    'system_notification' => 'CodersStudio\Notifications\Notifications\System'
 ];
