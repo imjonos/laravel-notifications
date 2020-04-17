2e3b7e03663095c94bd24ccac66c328cd5c0c8db
Eugeny Nosenko <info@coders.studio>
2020-04-17 19:52:17 +0300

    notifiable
    count added

--- src/Notifications.php
+++ src/Notifications.php
@@ -2,20 +2,35 @@
 
 namespace CodersStudio\Notifications;
 use Notification;
+use Illuminate\Support\Collection;
 
 class Notifications
 {
     /**
      * Send message to notifications
      *
-     * @param integer $user
+     * @param Illuminate\Support\Collection $notifiable
      * @param string $message
      * @param string $link
      * @return void
      */
-    public function send($user, $message = "", $link = "/"):void
+    public function send(Collection $notifiable, $message = "", $link = "/"):void
     {
         $class = config('notifications.system_notification');
-        Notification::send($user, new $class($message, $link));
+        Notification::send($notifiable, new $class($message, $link));
     }
+    
+    /**
+     * Count unread messages notifications
+     *
+     * @param Illuminate\Support\Collection $notifiable
+     * @return integer
+     */
+    public function count(Collection $notifiable, $message = "", $link = "/"):integer
+    {
+        $class = config('notifications.system_notification');
+        return $notifiable->unreadNotifications()->where('type', $class)->count();
+    }
+    
+   
 }
